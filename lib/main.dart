bd1a0e7b11368a27d7a39effb2513a6f106d7cc6
laianapinho <laiana.cavalcante15@gmail.com>
2026-07-10 21:12:01 -0400

    refactor: limpa codigo inicial e melhora layout

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Rastreador de Hábitos',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
       home: const MyHomePage(title: 'Meus Hábitos'),
@@ -38,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,42 +29,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   final List<Map<String, dynamic>> habitos = [
-  {'nome': 'Beber água', 'feito': false},
-  {'nome': 'Ler', 'feito': false},
-  {'nome': 'Exercitar', 'feito': false},
-  {'nome': 'Estudar Flutter', 'feito': false},
-];
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+    {'nome': 'Beber água', 'feito': false},
+    {'nome': 'Ler', 'feito': false},
+    {'nome': 'Exercitar', 'feito': false},
+    {'nome': 'Estudar Flutter', 'feito': false},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar  .
         title: Text(widget.title),
       ),
       body: Padding(
@@ -97,14 +48,21 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Hoje'),
+            Text('Hoje', style: Theme.of(context).textTheme.headlineMedium),
             SizedBox(height: 8),
-            Text('Acompanhe seus hábitos diários'),
+            Text(
+              'Acompanhe seus hábitos diários',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
             SizedBox(height: 24),
             ...habitos.map((habito) {
               return Container(
                 padding: EdgeInsets.all(16),
                 margin: EdgeInsets.only(bottom: 12),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surfaceContainerHighest,
+                  borderRadius: BorderRadius.circular(16),
+                ),
                 child: Row(
                   children: [
                     Checkbox(
@@ -123,12 +81,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               );
             }).toList(),
-  ],
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          print('Adicionar novo hábito');
+        },
+        tooltip: 'Adicionar hábito',
         child: const Icon(Icons.add),
       ),
     );
